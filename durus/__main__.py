08b9ed766e77819c2965298f3130f564ef5b829a
Christian Tismer <tismer@stackless.com>
2013-06-29 00:36:58 +0200

    improve help and move to argparse

--- durus/__main__.py
+++ durus/__main__.py
@@ -12,7 +12,7 @@ from durus.storage_server import DEFAULT_PORT, DEFAULT_HOST, DEFAULT_GCBYTES
 from durus.storage_server import SocketAddress
 from durus.storage_server import StorageServer, wait_for_server
 from durus.utils import int8_to_str, str_to_int8, write
-from optparse import OptionParser
+from argparse import ArgumentParser
 from pprint import pprint
 from time import sleep
 from types import ModuleType
@@ -71,51 +71,51 @@ def interactive_client(file, address, cache_size, readonly, repair,
     console.interact('Durus %s\n%s' % (description, help))
 
 def client_main():
-    from optparse import OptionParser
-    parser = OptionParser()
-    parser.set_description("Opens a client connection to a Durus server.")
-    parser.add_option(
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=
+                            "Opens a client connection to a Durus server.")
+    parser.add_argument(
         '--file', dest="file", default=None,
         help="If this is not given, the storage is through a Durus server.")
-    parser.add_option(
+    parser.add_argument(
         '--port', dest="port", default=DEFAULT_PORT,
-        type="int",
+        type=int,
         help="Port the server is on. (default=%s)" % DEFAULT_PORT)
-    parser.add_option(
+    parser.add_argument(
         '--host', dest="host", default=DEFAULT_HOST,
         help="Host of the server. (default=%s)" % DEFAULT_HOST)
-    parser.add_option(
+    parser.add_argument(
         '--address', dest="address", default=None,
         help=(
             "Address of the server.\n"
             "If given, this is the path to a Unix domain socket for "
             "the server."))
-    parser.add_option(
-        '--cache_size', dest="cache_size", default=10000, type="int",
+    parser.add_argument(
+        '--cache_size', dest="cache_size", default=10000, type=int,
         help="Size of client cache (default=10000)")
-    parser.add_option(
+    parser.add_argument(
         '--repair', dest='repair', action='store_true',
         help=('Repair the filestorage by truncating to remove anything '
               'that is malformed.  Without this option, errors '
               'will cause the program to report and terminate without '
               'attempting any repair.'))
-    parser.add_option(
+    parser.add_argument(
         '--readonly', dest='readonly', action='store_true',
         help='Open the file in read-only mode.')
-    parser.add_option(
+    parser.add_argument(
         '--startup', dest='startup',
         default=os.environ.get('DURUSSTARTUP', ''),
         help=('Full path to a python startup file to execute on startup.'
               '(default=DURUSSTARTUP from environment, if set)')
         )
-    (options, args) = parser.parse_args()
-    if options.address is None:
-        address = (options.host, options.port)
+    args = parser.parse_args()
+    if args.address is None:
+        address = (args.host, args.port)
     else:
-        address = options.address
-    interactive_client(options.file, address,
-                       options.cache_size, options.readonly, options.repair,
-                       options.startup)
+        address = args.address
+    interactive_client(args.file, address,
+                       args.cache_size, args.readonly, args.repair,
+                       args.startup)
 
 def get_storage(file, repair, readonly):
     if file:
@@ -155,101 +155,99 @@ def stop_durus(address):
     return True
 
 def run_durus_main():
-    parser = OptionParser()
-    parser.set_description('Run a Durus Server')
-    parser.add_option(
-        '--port', dest='port', default=DEFAULT_PORT, type='int',
+    parser = ArgumentParser('Run a Durus Server')
+    parser.add_argument(
+        '--port', dest='port', default=DEFAULT_PORT, type=int,
         help='Port to listen on. (default=%s)' % DEFAULT_PORT)
-    parser.add_option(
+    parser.add_argument(
         '--file', dest='file', default=None,
         help=('If not given, the storage is in a new temporary file.'))
-    parser.add_option(
+    parser.add_argument(
         '--host', dest='host', default=DEFAULT_HOST,
         help='Host to listen on. (default=%s)' % DEFAULT_HOST)
-    parser.add_option(
-        '--gcbytes', dest='gcbytes', default=DEFAULT_GCBYTES, type='int',
+    parser.add_argument(
+        '--gcbytes', dest='gcbytes', default=DEFAULT_GCBYTES, type=int,
         help=('Trigger garbage collection after this many commits. (default=%s)' %
             DEFAULT_GCBYTES))
     if hasattr(socket, 'AF_UNIX'):
-        parser.add_option(
+        parser.add_argument(
             '--address', dest="address", default=None,
             help=(
                 "Address of the server.\n"
                 "If given, this is the path to a Unix domain socket for "
                 "the server."))
-        parser.add_option(
+        parser.add_argument(
             '--owner', dest="owner", default=None,
             help="Owner of the Unix domain socket (the --address value).")
-        parser.add_option(
+        parser.add_argument(
             '--group', dest="group", default=None,
             help="group of the Unix domain socket (the --address value).")
-        parser.add_option(
-            '--umask', dest="umask", default=None, type="int",
+        parser.add_argument(
+            '--umask', dest="umask", default=None, type=int,
             help="umask for the Unix domain socket (the --address value).")
     logginglevel = logger.getEffectiveLevel()
-    parser.add_option(
-        '--logginglevel', dest='logginglevel', default=logginglevel, type='int',
+    parser.add_argument(
+        '--logginglevel', dest='logginglevel', default=logginglevel, type=int,
         help=('Logging level. Lower positive numbers log more. (default=%s)' %
               logginglevel))
-    parser.add_option(
+    parser.add_argument(
         '--logfile', dest='logfile', default=None,
         help=('Log file. (default=stderr)'))
-    parser.add_option(
+    parser.add_argument(
         '--repair', dest='repair', action='store_true',
         help=('Repair the filestorage by truncating to remove anything '
               'that is malformed.  Without this option, errors '
               'will cause the program to report and terminate without '
               'attempting any repair.'))
-    parser.add_option(
+    parser.add_argument(
         '--readonly', dest='readonly', action='store_true',
         help='Open the file in read-only mode.')
-    parser.add_option(
+    parser.add_argument(
         '--stop', dest='stop', action='store_true',
         help='Instead of starting the server, try to stop a running one.')
-    (options, args) = parser.parse_args()
-    if getattr(options, 'address', None) is None:
-        address = SocketAddress.new((options.host, options.port))
+    args = parser.parse_args()
+    if getattr(args, 'address', None) is None:
+        address = SocketAddress.new((args.host, args.port))
     else:
-        address = SocketAddress.new(address=options.address,
-            owner=options.owner, group=options.group, umask=options.umask)
-    if not options.stop:
-        start_durus(options.logfile,
-                    options.logginglevel,
+        address = SocketAddress.new(address=args.address,
+            owner=args.owner, group=args.group, umask=args.umask)
+    if not args.stop:
+        start_durus(args.logfile,
+                    args.logginglevel,
                     address,
-                    get_storage(options.file, options.repair, options.readonly),
-                    options.gcbytes)
+                    get_storage(args.file, args.repair, args.readonly),
+                    args.gcbytes)
     else:
         stop_durus(address)
 
 def pack_storage_main():
-    parser = OptionParser()
-    parser.set_description("Packs a Durus storage.")
-    parser.add_option(
+    parser = ArgumentParser("Packs a Durus storage.")
+    parser.add_argument(
         '--file', dest="file", default=None,
         help="If this is not given, the storage is through a Durus server.")
-    parser.add_option(
+    parser.add_argument(
         '--port', dest="port", default=DEFAULT_PORT,
-        type="int",
+        type=int,
         help="Port the server is on. (default=%s)" % DEFAULT_PORT)
-    parser.add_option(
+    parser.add_argument(
         '--host', dest="host", default=DEFAULT_HOST,
         help="Host of the server. (default=%s)" % DEFAULT_HOST)
-    (options, args) = parser.parse_args()
-    if options.file is None:
-        wait_for_server(options.host, options.port)
-        storage = ClientStorage(host=options.host, port=options.port)
+    args = parser.parse_args()
+    if args.file is None:
+        wait_for_server(args.host, args.port)
+        storage = ClientStorage(host=args.host, port=args.port)
     else:
-        storage = FileStorage(options.file)
+        storage = FileStorage(args.file)
     connection = Connection(storage)
     connection.pack()
 
 def usage():
     sys.stdout.write(
-        'durus [ -c | -s | -p ] [ -h ] [<specific options>]\n'
-        '    -s   Start or stop a Durus storage server.\n'
-        '    -c   Start a low-level interactive client.\n'
-        '    -p   Pack a storage file.\n'
-        '    -h   Get help on specific options.\n')
+        'durus [ -c | -s | -p ] [ -h|--help ] [<specific options>]\n'
+        '    -s | server  Start or stop a Durus storage server.\n'
+        '    -c | client  Start a low-level interactive client.\n'
+        '    -p | pack    Pack a storage file.\n'
+        'add --help for help on specific options.\n')
 
 def main():
     if len(sys.argv) == 1:
@@ -257,11 +255,11 @@ def main():
     else:
         arg = sys.argv[1]
         sys.argv[1:] = sys.argv[2:]
-        if arg == '-c':
+        if arg in ('-c', 'client', '--client'):
             client_main()
-        elif arg == '-s':
+        elif arg in ('-s', 'server', '--server'):
             run_durus_main()
-        elif arg == '-p':
+        elif arg in ('-p', 'pack', '--pack'):
             pack_storage_main()
         else:
             usage()
