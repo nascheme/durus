3dd2daf8202a9971b85efda0197b7760016c915a
Neil Schemenauer <nas@arctrix.com>
2014-02-07 10:16:50 -0600

    Restore old behavior of determining cache shrink size.
    I'm not sure if the restored code is very good but using
    
        (len(all) - self.size) * 2
    
    results in the shrink often doing too much work and introducing long
    delays if you have a large cache.

--- durus/connection.py
+++ durus/connection.py
@@ -438,7 +438,8 @@ class Cache (object):
         """(transaction_serial:int) -> [(serial, oid)]
         """
         all = self.objects
-        heap_size_target = (len(all) - self.size) * 2
+        n = len(all)
+        heap_size_target = max(min(self.size - n, n // 4), n // 64)
         start = self.finger % len(all)
         heap = []
         for oid in islice(chain(all, all), start, start + len(all)):
